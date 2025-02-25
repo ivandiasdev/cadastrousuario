24c5ede16e94a0302901cf291d437c6cb90dc0f8
Ivan Dias <117956928+ivandiasdev@users.noreply.github.com>
2025-02-25 13:53:51 -0300

    Atualização de API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://apicadastro-feps.onrender.com/',
     headers: {
         'Authorization': 'Bearer YOUR_API_TOKEN',
     },
